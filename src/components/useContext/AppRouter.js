3704721d2c9c354d8a9af1ef5c79848970277cb0
LAPTOP-PJ1275UD\Matias <matias.angeluk@gmail.com>
2022-03-31 22:15:08 -0300

    useContext

--- src/components/useContext/AppRouter.js
+++ src/components/useContext/AppRouter.js
@@ -4,14 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
   return (
         <BrowserRouter>
+
+            <NavBar/>
+
             <Routes>
                 <Route path="/" element={<HomeScreen/>}/>
                 <Route path="about" element={<AboutScreen/>}/>
                 <Route path="login" element={<LoginScreen/>}/>
+                <Route path="*" element={<HomeScreen/>}/>
             </Routes>
         </BrowserRouter>
   )
--- src/components/useContext/MainApp.js
+++ src/components/useContext/MainApp.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
+
+  const [user, setUser] = useState({});
+
   return (
-      <AppRouter/>
+      <UserContext.Provider value={{
+        user,
+        setUser,
+        temporal:1234
+      }}>
+        <AppRouter/>
+      </UserContext.Provider>
   )
 }
