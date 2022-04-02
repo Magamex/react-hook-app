19788e19fc29f7a425224b19673d4a68a2254b16
LAPTOP-PJ1275UD\Matias <matias.angeluk@gmail.com>
2022-04-02 00:06:55 -0300

    diferentes test con jest de hook

---
+++ src/tests/HookApp.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HookApp } from './../HookApp';
+import { shallow } from 'enzyme';
+
+describe('Pruebas en <HookApp/>', () => {
+    test('debe mostrarse correctamente', () => {
+        const wrapper = shallow(<HookApp/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
+
